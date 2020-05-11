af03efaaa4a96d96cf60974f0d7b3303f4bfa5aa
MinSeok Kim <myminseok@gmail.com>
2020-05-12 00:35:37 +0900

    terraform

--- tasks/prepare-credhub-secrets-from-terraform-state.sh
+++ tasks/prepare-credhub-secrets-from-terraform-state.sh
@@ -63,19 +63,18 @@ function set_ssh_private_key(){
 }
 
 
-set +e
-
 ## for opsman.yml , director.yml
 set_first_value_from_array "public_subnet_ids" ##  module.infra.public_subnet_ids, 0
 set_value "ops_manager_security_group_id"
 set_value "ops_manager_ssh_public_key_name"
 set_value "ops_manager_iam_instance_profile_name"
 set_value "ops_manager_public_ip"
-set_value "ops_manager_iam_user_access_key"
-set_password "ops_manager_iam_user_secret_key"
-set_value "rds_address"
-set_value "rds_username"
-set_password "rds_password"
 set_ssh_private_key "ops_manager_ssh_private_key"
+
+##set_value "ops_manager_iam_user_access_key"
+##set_password "ops_manager_iam_user_secret_key"
+#set_value "rds_address"
+#set_value "rds_username"
+#set_password "rds_password"
 
 

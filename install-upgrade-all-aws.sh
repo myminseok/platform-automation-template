4111f54806072d7f539791ac8ee85469650dc2ea
MinSeok Kim <myminseok@gmail.com>
2020-05-14 23:37:43 +0900

    test

--- install-upgrade-all-aws.sh
+++ install-upgrade-all-aws.sh
@@ -10,6 +10,6 @@ FLY_TARGET=$1
 
 fly -t ${FLY_TARGET} sp -p "${FLY_TARGET}-install-upgrade-all" \
 -c ./install-upgrade-all-aws.yml \
--l ./${FLY_TARGET}/pipeline-vars/params.yml
+-l ../platform-automation-configuration-template/${FLY_TARGET}/pipeline-vars/params.yml
 
 ## fly -t demo clear-task-cache 

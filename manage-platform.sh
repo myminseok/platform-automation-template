5f1903ba7031f017dd5ad155fdf6f6c10603593b
MinSeok Kim <myminseok@gmail.com>
2020-05-23 00:56:09 +0900

    fix

--- manage-platform.sh
+++ manage-platform.sh
@@ -12,5 +12,6 @@ FOUNDATION=$2
 
 fly -t ${FLY_TARGET} sp -p "${FOUNDATION}-manage-platform" \
 -c ./manage-platform.yml \
+-v foundation=${FOUNDATION} \
 -l ${FOUNDATION}/pipeline-vars/params.yml
 #-l ../platform-automation-configuration-template/${FOUNDATION}/pipeline-vars/params.yml

f6f8e69b9f3b4b8af650519b263630e418f5731b
MinSeok Kim <myminseok@gmail.com>
2020-05-24 13:41:25 +0900

    test

--- bbr-scripts/test-ssh-backup.sh
+++ bbr-scripts/test-ssh-backup.sh
@@ -6,10 +6,6 @@ current_date="$( date +"%Y-%m-%d-%H-%M-%S" )"
 
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "WORK_DIR/env.yml test" $WORK_DIR/env.yml
-
-cat $WORK_DIR/env.yml
-
 BOSH_ENVIRONMENT="1.1.1.1"
 export BOSH_BBR_ACCOUNT=bbr
 export BACKUP_FILE="${BOSH_ENVIRONMENT}_director-backup_${current_date}.tgz"

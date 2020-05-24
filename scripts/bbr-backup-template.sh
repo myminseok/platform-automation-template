093e51380e1fc3c39c96486726641023e1c315e2
MinSeok Kim <myminseok@gmail.com>
2020-05-24 12:58:58 +0900

    test

--- scripts/bbr-backup-template.sh
+++ scripts/bbr-backup-template.sh
@@ -14,7 +14,9 @@ BOSH_ENVIRONMENT="1.1.1.1"
 export BOSH_BBR_ACCOUNT=bbr
 export BACKUP_FILE="${BOSH_ENVIRONMENT}_director-backup_${current_date}.tgz"
 
-TMP_DIR="$WORK_DIR/${BASH_SOURCE[0]}_${current_date}"
+SCRIPT_NAME=$(basename ${BASH_SOURCE[0]})
+TMP_DIR="$WORK_DIR/${SCRIPT_NAME}_${current_date}"
+echo $TMP_DIR
 mkdir -p $TMP_DIR
 pushd $TMP_DIR
 

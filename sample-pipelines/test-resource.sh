1afa1bd868da6338309f970febb2a5d76e348604
MinSeok Kim <myminseok@gmail.com>
2020-06-09 14:21:49 +0900

    fix

--- sample-pipelines/test-resource.sh
+++ sample-pipelines/test-resource.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ -z $1 ] ; then
+if [ -z $1 ] ||  [ -z $2 ] ; then
     echo "!!! please provide parameters"
-	echo "${BASH_SOURCE[0]} [fly-target]"
+	echo "${BASH_SOURCE[0]} [fly-target] FOUNDATION"
 	exit
 fi	
 FLY_TARGET=$1
+FOUNDATION=$2
 
 fly -t ${FLY_TARGET} sp -p test-resources -c ./test-resources.yml \
--l ../${FOUNDATION}/pipeline-vars/params.yml
+-v foundation=${FOUNDATION} \
+-l ../envs/${FOUNDATION}/pipeline-vars/params.yml

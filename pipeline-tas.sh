df494af0cce25f0d36dd1dd2759ce91077d3979f
MinSeok Kim <myminseok@gmail.com>
2020-06-10 13:03:01 +0900

    ytt

--- pipeline-tas.sh
+++ pipeline-tas.sh
@@ -9,9 +9,9 @@ if [ -z $1 ] || [ -z $2 ] ; then
 fi	
 FLY_TARGET=$1
 FOUNDATION=$2
+PIPELINE_NAME="${FOUNDATION}-tas"
 
-
-## https://github.com/k14s/ytt/releases
+## ytt from https://github.com/k14s/ytt/releases
 ./ytt template \
     -f pipeline-template/pipeline-tas.yml \
     -f pipeline-template/resource_types.lib.yml \
@@ -22,8 +22,8 @@ FOUNDATION=$2
     -f pipeline-template/tas_products_jobs.lib.yml \
     --ignore-unknown-comments > ./pipeline-tas-generated.yml
 
-fly -t ${FLY_TARGET} sp -p "${FOUNDATION}-manage-platform" \
+fly -t ${FLY_TARGET} sp -p "${PIPELINE_NAME}" \
 -c ./pipeline-tas-generated.yml \
 -v foundation=${FOUNDATION} \
--v BUILD_PIPELINE_NAME="${FOUNDATION}-manage-platform"  \
+-v pipeline_name="${PIPELINE_NAME}"  \
 -l ./envs/${FOUNDATION}/pipeline-vars/params.yml

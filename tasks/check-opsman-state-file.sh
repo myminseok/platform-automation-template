0a86061ff9e4855958a6837f7a5d44aae366efcf
MinSeok Kim <myminseok@gmail.com>
2020-06-10 12:03:50 +0900

    ytt

--- tasks/check-opsman-state-file.sh
+++ tasks/check-opsman-state-file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## when create-vm, check if there is a config/state/state.yml on git and matches with 'vm_name' on configs/opsman.yml
 
+
 state_file="config/$STATE_FILE"
 if [[ ! -f $state_file ]]; then
   echo "OK, $state_file not exist"
@@ -12,14 +13,15 @@ if [[ ! -f $opsman_file ]]; then
   exit 1
 fi
 
-export vm_name_from_opsman_file=`grep "vm_name" $opsman_file | awk -F':' '{print $2}' |sed 's/ //g'`
+vm_name_from_opsman_file=`grep "vm_name" $opsman_file | awk -F':' '{print $2}' |sed 's/ //g'`
+vm_name_from_opsman_file=`echo $vm_name_from_opsman_file | awk -F'#' '{print $1}'`
 echo "vm_name: $vm_name_from_opsman_file"
 if [ -z "$vm_name_from_opsman_file" ]; then
   echo "Error, $vm_name_from_opsman_file is empty"
   exit 1
 fi
 
-export vm_id_from_state_file=`grep vm_id $state_file | awk -F'/' '{print $NF}' |sed 's/ //g'`
+vm_id_from_state_file=`grep vm_id $state_file | awk -F'/' '{print $NF}' |sed 's/ //g'`
 echo "vm_id: $vm_id_from_state_file"
 if [ -z "$vm_id_from_state_file" ]; then
   echo "OK, $state_file is empty"

72cda43a918035c87dd84f08bdc66e5594ebe2c2
MinSeok Kim <myminseok@gmail.com>
2020-06-10 22:22:04 +0900

    ytt

---
+++ pks-pipeline.sh
@@ -0,0 +1,34 @@
+
+
+#!/bin/bash
+
+if [ -z $1 ] || [ -z $2 ] ; then
+    echo "!!! please provide parameters"
+	echo "${BASH_SOURCE[0]} [fly-target] [foundation]"
+	exit
+fi
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FLY_TARGET=$1
+FOUNDATION=$2
+PIPELINE_NAME="${FOUNDATION}-pks-platform"
+PIPELINE_YAML="generated-pipeline-pks.yml"
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+## ytt from https://github.com/k14s/ytt/releases
+$WORK_DIR/ytt template \
+    -f $WORK_DIR/pipeline-template/pipeline-pks.yml \
+    -f $WORK_DIR/pipeline-template/resource_types.lib.yml \
+    -f $WORK_DIR/pipeline-template/common_resources.lib.yml \
+    -f $WORK_DIR/pipeline-template/opsman_groups.lib.yml \
+    -f $WORK_DIR/pipeline-template/opsman_resources.lib.yml \
+    -f $WORK_DIR/pipeline-template/opsman_jobs.lib.yml \
+    -f $WORK_DIR/pipeline-template/pks_products_groups.lib.yml \
+    -f $WORK_DIR/pipeline-template/pks_products_resources.lib.yml \
+    -f $WORK_DIR/pipeline-template/pks_products_jobs.lib.yml \
+    --ignore-unknown-comments > $WORK_DIR/${PIPELINE_YAML}
+
+fly -t ${FLY_TARGET} sp -p "${PIPELINE_NAME}" \
+-c $WORK_DIR/${PIPELINE_YAML} \
+-v foundation=${FOUNDATION} \
+-v pipeline_name="${PIPELINE_NAME}"  \
+-l $WORK_DIR/envs/${FOUNDATION}/pipeline-vars/params.yml

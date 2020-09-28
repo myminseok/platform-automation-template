a7c47fe15462ce7fc14cc37f19194d057e9df947
MinSeok Kim <myminseok@gmail.com>
2020-09-28 17:14:54 +0900

    vsphere-dev

---
+++ foundations/vsphere-dev/pipeline-vars/set-credhub.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+    echo "please provide parameters"
+	echo "${BASH_SOURCE[0]} PIPELINE_NAME"
+	exit
+fi	
+
+PIPELINE_NAME=$1	
+PREFIX='/concourse/main'
+
+
+credhub set -t value -n /concourse/main/s3_access_key_id -v ''
+credhub set -t password -n /concourse/main/s3_secret_access_key -w ''
+credhub set -t user -n /concourse/main/vcenter_user -z admin@vcenter.local -w "PASSWORD"
+credhub set -t password -n /concourse/main/pivnet_token -w ''
+credhub set -t value -n /concourse/main/git_user_email -v admin@user.io
+credhub set -t value -n /concourse/main/git_user_username -v ''
+credhub set -t user -n /concourse/main/smtp_user -z user -w 'secret'
+
+## register ssh key for git. ex) ~/.ssh/id_rsa
+credhub set -t rsa  -n /concourse/main/git_private_key  -p ~/.ssh/id_rsa
+ 
+
+## cd concourse-bosh-deployment/cluster
+## bosh int ./concourse-creds.yml --path /atc_tls/certificate > atc_tls.cert
+## bosh int ./credhub-vars-store.yml --path=/credhub-ca/ca > credhub-ca.ca
+# credhub set -t certificate -n /concourse/main/credhub_ca_cert -c ./credhub-ca.ca
+## grep concourse_to_credhub ./concourse-creds.yml
+credhub set -t user -n /concourse/main/credhub_client -z concourse_client -w 'secret'
+credhub set -t user  -n ${PREFIX}/${PIPELINE_NAME}/opsman_admin -z admin -w 'PASSWORD'
+credhub set -t password -n ${PREFIX}/${PIPELINE_NAME}/decryption-passphrase -w 'PASSWORD'
+credhub set -t value -n ${PREFIX}/${PIPELINE_NAME}/opsman_target -v "https://"
+# for opsman.yml on vsphere
+credhub set -t rsa  -n ${PREFIX}/${PIPELINE_NAME}/opsman_ssh_key -u ~/.ssh/id_rsa.pub -p ~/.ssh/id_rsa
+
+##*.awstest.pcfdemo.net,*.sys.awstest.pcfdemo.net,*.apps.awstest.pcfdemo.net,*.uaa.sys.awstest.pcfdemo.net,*.login.sys.awstest.pcfdemo.net
+credhub set -t certificate -n ${PREFIX}/${PIPELINE_NAME}/tas_ssl_domain -c ./tas_ssl_domain.crt -p ./tas_ssl_domain.key
+
+## bosh credhub : credhub get -n /services/tls_ca -k ca
+## TAS tile> networking> domain certifiate
+credhub set -t certificate -n ${PREFIX}/${PIPELINE_NAME}/director_trusted_certificates -c ./director_trusted_certificates
+
+##grafana.pcfdemo.net
+credhub set -t certificate -n ${PREFIX}/${PIPELINE_NAME}/grafana_ssl_certificates -c ./grafana_ssl_certificates.crt -p ./grafana_ssl_certificates.key
+
+credhub set -t password -n ${PREFIX}/${PIPELINE_NAME}/properties_grafana_authentication_uaa_client_secret -w 'PASSWORD'
+
+credhub set -t user -n ${PREFIX}/${PIPELINE_NAME}/bosh-deployments-exporter_bosh_client -z 'admin' -w "PASSWORD"
+
